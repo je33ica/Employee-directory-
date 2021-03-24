13a976a1eb575c83e286e57b0745376e9dbaa91a
je33ica <je331687@gmail.com>
2021-03-24 19:33:09 +0000

    added started code for react

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>Employee management system </h1>
+      </div>
+    );
+  }
+}
