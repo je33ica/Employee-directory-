40080e62c1fc054795a926cefc0e214ea93e4f34
je33ica <je331687@gmail.com>
2021-03-27 17:56:17 +0000

    working on table area- now making api call and displaying employees data- only 1 for now

---
+++ src/components/TableArea.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+const TableArea = ({ employees }) => {
+  return (
+    <div>
+      {employees.length > 0 ? (
+        <p>
+          {employees[0].name.first} {employees[0].name.last}
+        </p>
+      ) : (
+        <p>Loading Data...</p>
+      )}
+    </div>
+  );
+
+  //make a table - bootstap
+  //head- top row with column defintions
+  //table body, map employees for each employee we will make a row
+  //pass props into each row
+  //each row will be a component
+
+  // const [employee, setEmployee] = useState()
+  // declare state and this is where the employee data.
+  // We know we need employees and filtered employees
+  //handleSearch function - takes the inchange event from the nav and filters the employees
+  //handleSort function - this is going to be passed to the table
+  //component did mount should make a call to the api and return the data
+  //that data is going to be set in state
+  //inside the render we will have the navbar which is going to be passed a handleSearch function on props
+  //another component called Table which will take the employees object and handle sort function
+};
+export default TableArea;
+
+//results.name.title
+//results.name.first
+//results.name.last
+//results.location.city
+//results.location.country
+//results.email
+//results.'d.o.b'.date
+//results.'d.o.b'.age
+//reults.picture.thumbnail
