9aa97f4e23df76372f4bab69d78b8d1345cd5e92
je33ica <je331687@gmail.com>
2021-05-04 18:51:32 +0100

    refactored code

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,20 +1,18 @@
 const Row = ({ employee }) => {
   return (
     <tr>
-      <th scope="row" key={employee.login.uuid}>
-        <td>
-          {employee.name.first} &nbsp;
-          {employee.name.last}
-        </td>
-        <td>
-          <img alt="User-Profile" src={employee.picture.thumbnail} />
-        </td>
-        <td>{employee.email}</td>
-        <td>
-          {employee.location.city}, {employee.location.country}
-        </td>
-        <td>{employee.dob.age}</td>
-      </th>
+      <td key={employee.login.uuid}>
+        {employee.name.first} &nbsp;
+        {employee.name.last}
+      </td>
+      <td>
+        <img alt="User-Profile" src={employee.picture.thumbnail} />
+      </td>
+      <td>{employee.email}</td>
+      <td>
+        {employee.location.city}, {employee.location.country}
+      </td>
+      <td>{employee.dob.age}</td>
     </tr>
   );
 };
